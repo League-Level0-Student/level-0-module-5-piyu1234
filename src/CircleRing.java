b1d4e6643a7116a7f8fbf69666187fdc045ef436
piyu1234 <league@WTS14.attlocal.net>
2018-04-01 17:27:45 -0700

    module 5 yay

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -6,29 +7,37 @@
 import org.jointheleague.graphical.robot.Robot;
 
 public class CircleRing {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        // 1. Make a new Robot
+		// 1. Make a new Robot
+		Robot circle = new Robot();
 
+		// 2. Set your robot’s position to x=150 and y=200
+		circle.setX(150);
+		circle.setY(200);
+		// 3. Put the robot's pen down
+		circle.penDown();
+		// 4. Set the robot’s speed to 10
+		circle.setSpeed(10);
+		// 5. Do everything below here 360 times (use i as the counter)
+		for (int i = 0; i < 360; i++) {
 
-       //  2. Set your robot’s position to x=150 and y=200
+			// 6. Move the robot 3 pixels
+			circle.move(3);
+			// 7. Turn the robot 1 degree
+			circle.turn(1);
+			// 8. If the counter i is divisible by 20 (hint: use mod operator %)..
+			if (i % 20 == 0) {
 
-        // 3. Put the robot's pen down
+				// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
+				for (int j = 0; j < 360; j++) {
 
-        // 4. Set the robot’s speed to 10
-
-        // 5. Do everything below here 360 times (use i as the counter)
-
-	        // 6. Move the robot 3 pixels
-
-       //  7. Turn the robot 1 degree
-
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
-
-		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
-
-				// 10. Move the robot 1 pixel
-
-				// 11. Turn the robot 1 degree
-}
+					// 10. Move the robot 1 pixel
+					circle.move(1);
+					// 11. Turn the robot 1 degree
+					circle.turn(1);
+				}
+			}
+		}
+	}
 }
