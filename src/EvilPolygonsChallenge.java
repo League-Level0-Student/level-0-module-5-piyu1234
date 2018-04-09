1f0e16c28e1f63161192c5cf0080443a7309c4d4
Cainnan Carra <league@WTS10.attlocal.net>
2018-04-08 17:38:20 -0700

    almost don with level o

--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -8,17 +8,47 @@ public class EvilPolygonsChallenge {
 	
 	public static void main(String[] args) {
 		//1. Create a robot
+		Robot polygon = new Robot();
 
 		//2. Set the speed to 100
-
+polygon.setSpeed(100);
 		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
 		
 		//3. Set the pen color of the robot according to the value of the colorChoise variable
-		
+		polygon.setPenColor(colorChoice, colorChoice, colorChoice);
 		//4. Ask the use how many polygons they want to be drawn.
-		
+String numberword =		JOptionPane.showInputDialog("How many polygons do you want to be drawn?");
+int number;
+number = Integer.parseInt(numberword);
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+for (int i = 0; i < number; i++) {
+	
+
+	polygon.move(colorChoice);
+	polygon.penDown();
+	polygon.move(80);
+	polygon.turn(40);
+	polygon.move(80);
+	polygon.turn(40);
+	polygon.move(80);
+	polygon.turn(40);
+	polygon.move(80);
+	polygon.turn(40);
+	polygon.move(80);
+	polygon.turn(40);
+	polygon.move(80);
+	polygon.turn(40);
+	polygon.move(80);
+	polygon.turn(40);
+	polygon.move(80);
+	polygon.turn(40);
+	polygon.move(80);
+	polygon.turn(40);
+	polygon.move(80);
+	polygon.turn(40);
+	polygon.move(80);
+}
+	
 		//6. Make it so your shapes do not overlap
 
 		//7. Challenge: add more colors to the Option Dialog.
--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -10,12 +10,20 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+String Name = JOptionPane.showInputDialog("Enter your name.");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+System.out.println(Name.toUpperCase());  
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
+String newword = ""; 
+for (int i = 0; i <Name.length(); i++) {
+if  (  i%2 > 0) 
+{  newword.concat(Name.substring(i, i+1).toUpperCase());
+	}
+
+System.out.println(newword);
+}
 
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
@@ -27,9 +35,11 @@ public class GoofyNames {
 			
 			// 6. ADD this String  (containing 1 char) to the goofyName String
 
-		
-		// 7. Use pop-up to show user their Goofy name
+	}
 
+	private static Object toUpperCase() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
 
